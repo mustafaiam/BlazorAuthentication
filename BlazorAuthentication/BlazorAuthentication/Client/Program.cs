13a4f6f77d91357e42afddd18af4b6dcd5294c7f
mustafaiam <mustafaiam.github@gmail.com>
2022-04-11 01:34:43 +0300

    Implemented minor changes to the code.
    Couldn't complete the whole plan for improvement of the code, but I will continue tomorrow.

--- BlazorAuthentication/BlazorAuthentication/Client/Program.cs
+++ BlazorAuthentication/BlazorAuthentication/Client/Program.cs
@@ -1,5 +1,6 @@
 global using Microsoft.AspNetCore.Components.Authorization;
 global using System.Security.Claims;
+global using Blazored.LocalStorage;
 using BlazorAuthentication.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -11,5 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
--- BlazorAuthentication/BlazorAuthentication/Server/Program.cs
+++ BlazorAuthentication/BlazorAuthentication/Server/Program.cs
@@ -1,3 +1,4 @@
+global using BlazorAuthentication.Shared;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
